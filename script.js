badad769a6a6b032a0c4075adb1681153750f93f
dostendite <dostendite@outlook.es>
2023-09-11 01:40:28 +0200

    Add basic etch-a-sketch functionality
    I managed to add the main sketch feature With JS DOM referencing,
    though the reset button's not working yet and I have to add the
    "Set Grid Size" function, which is probably gonna be harder
    than making the sketch functionality itself

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
 // create a 16x16 grid of square divs
 // using the DOM inside grid-container
 
+const gridContainer = document.querySelector(".grid-container");
+const setGridButton = document.querySelector("#set-grid-button");
+const resetButton = document.querySelector("#reset-button");
+// figure out a way to make the reset button work
+
+function addDivs() {
+    const newDiv = document.createElement("div");
+
+    gridContainer.appendChild(newDiv)
+
+}
+
+
+for (let i = 0; i <= 255; i++) {
+    addDivs();
+}
+
+// set up a "hover" efect so that the grid divs
+// change color when your mouse passes over them,
+// leaving a trail through your grid like a pen would
+
+const gridDivs = gridContainer.children;
+
+for (gridDiv of gridDivs) {
+    gridDiv.addEventListener("mouseenter", (event) => {
+        event.target.style.backgroundColor = "#9f59ff";
+    })
+}
