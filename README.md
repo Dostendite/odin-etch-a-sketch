5a521405ae44345326d1fbe34dbbe22f02d6dcba
dostendite <dostendite@outlook.es>
2023-09-11 03:04:40 +0200

    Add grid size button & finish base game

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
-# TOP Foundations Project No. 4
+# TOP No.4 - Etch-A-Sketch
 
-## Etch-A-Sketch
+## Missing features:
+1. Reset Button
+2. Rainbow Mode
+
+## Pending further styling / decoration
+
+The game's working now though!
--- script.js
+++ script.js
@@ -4,28 +4,70 @@
 const gridContainer = document.querySelector(".grid-container");
 const setGridButton = document.querySelector("#set-grid-button");
 const resetButton = document.querySelector("#reset-button");
-// figure out a way to make the reset button work
+const rainbowModeButton = document.querySelector("#rainbow-button")
 
-function addDivs() {
-    const newDiv = document.createElement("div");
-
-    gridContainer.appendChild(newDiv)
-
-}
-
-
-for (let i = 0; i <= 255; i++) {
-    addDivs();
-}
-
-// set up a "hover" efect so that the grid divs
-// change color when your mouse passes over them,
-// leaving a trail through your grid like a pen would
-
+const GRID_SIZE = 640;
 const gridDivs = gridContainer.children;
+// const rainbowMode = false;
 
-for (gridDiv of gridDivs) {
-    gridDiv.addEventListener("mouseenter", (event) => {
-        event.target.style.backgroundColor = "#9f59ff";
-    })
+setGridButton.addEventListener("click", setGrid);
+
+// rainbowModeButton.addEventListener("click", function () {
+//     rainbowMode = true;
+// })
+
+// colors = ["red", "blue", "lime", "yellow", "magenta", "cyan", "black", "white"];
+
+function getRandomColor() {
+
+    let randomNumber = ~~(Math.random() * 8)
+
+    return colors[randomNumber];
 }
+
+function setGrid(divsPerSide) {
+
+    if (divsPerSide === "default") {
+        divsPerSide = 16;
+    } else {
+
+        divsPerSide = parseInt(window.prompt(`Select number of squares
+        per side (4 to 100)`, 4))
+    }
+
+    // Total amount of squares to generate = ivsPerSide²
+
+    let totalDivs = (divsPerSide * divsPerSide) - 1;
+
+    // GRID_SIZE / divsPerSide = squareDimensions
+    let squareDimensions = `${GRID_SIZE / divsPerSide}px`;
+
+    // Remove all divs/squares from canvas 
+    gridContainer.innerHTML = "";
+
+    // Fill canvas with squares
+    for (let i = 0; i <= totalDivs; i++) {
+        addDiv();
+    }
+
+    for (gridDiv of gridDivs) {
+
+        gridDiv.style.width = squareDimensions;
+        gridDiv.style.height = squareDimensions;
+
+        // Trail / hover effect
+        gridDiv.addEventListener("mouseenter", (event) => {
+
+            event.target.style.backgroundColor = "#9f59ff";
+
+        })
+    }
+}
+
+function addDiv() {
+    const newDiv = document.createElement("div");
+    gridContainer.appendChild(newDiv);
+}
+
+// Default value
+setGrid("default");
